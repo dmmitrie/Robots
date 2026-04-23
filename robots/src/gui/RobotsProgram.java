3bc75cf406b0011a26874627ff836fa824794bf8
dmmitrie <ekst06@gmail.com>
2026-04-24 00:08:06 +0500

    доработка RobotsProgram. делаем код более адаптивным, те. язык подстраивается под локализацию java

--- robots/src/gui/RobotsProgram.java
+++ robots/src/gui/RobotsProgram.java
@@ -1,19 +1,13 @@
 package gui;
 
 import log.Logger;
+
 import javax.swing.*;
 
 public class RobotsProgram
 {
   public static void main(String[] args)
   {
-    UIManager.put("OptionPane.okButtonText", "OK");
-    UIManager.put("OptionPane.yesButtonText", "Да");
-    UIManager.put("OptionPane.noButtonText", "Нет");
-    UIManager.put("OptionPane.cancelButtonText", "Отмена");
-    UIManager.put("OptionPane.titleText", "Подтверждение");
-    UIManager.put("OptionPane.messageDialogTitle", "Подтверждение действия");
-
     try
     {
       UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
